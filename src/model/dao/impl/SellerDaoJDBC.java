72536747685507db6fe0f2ed0b5c3be6657f7237
Pedro Henrique <pedrohlucasdepaula@gmail.com>
2026-01-27 13:17:49 -0300

    findAll implements

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -92,12 +92,6 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Seller> findByDepartment(Department department) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
@@ -105,9 +99,7 @@ public class SellerDaoJDBC implements SellerDao {
 					"SELECT seller.*,department.Name as DepName "
 					+ "FROM seller INNER JOIN department "
 					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
 					+ "ORDER BY Name ");		
-			st.setInt(1, department.getId());
 			
 			rs = st.executeQuery();
 			
@@ -136,5 +128,46 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+			PreparedStatement st = null;
+			ResultSet rs = null;
+			try {
+				st = conn.prepareStatement(
+						"SELECT seller.*,department.Name as DepName "
+						+ "FROM seller INNER JOIN department "
+						+ "ON seller.DepartmentId = department.Id "
+						+ "WHERE DepartmentId = ? "
+						+ "ORDER BY Name ");		
+				st.setInt(1, department.getId());
+				
+				rs = st.executeQuery();
+				
+				List<Seller> list = new ArrayList<>();
+				Map<Integer, Department> map = new HashMap<>();
+				
+				while (rs.next()) {
+					
+					Department dep = map.get(rs.getInt("DepartmentId"));
+					
+					if (dep == null ) {
+						dep = instantiateDepartment(rs);
+						map.put(rs.getInt("DepartmentId"), dep);
+					}
+					
+					Seller obj = instantiateSeller(rs, dep);
+					list.add(obj);
+				}
+				return list;
+			}
+			catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			finally {
+				DB.closeStatement(st);
+				DB.closeResultSet(rs);
+			}
+		}
 
 }
